a0af48d9032297e23c4e99bdd01b36ec78b1bade
Bhargav Panth <bhargavrpanth@gmail.com>
2018-06-05 17:28:31 +0100

    add exception handling to convert character set to utf-8

--- job/Map/mapper.py
+++ job/Map/mapper.py
@@ -7,27 +7,32 @@ def mapper():
 	# document = HDTDocument(os.path.append("../../data/LOD_a_lot_v1.hdt"))
 	# standard in - file line by line
 	for each_line in sys.stdin:
-		# try to utf-8 encode - 
-		data = each_line.strip().split(' ')
-		
-		if len(data) == 4:
-			try:
-				sub, pred, obj, term = data
-			except Exception as e:
-				pass
-			else:
-				pass
-			finally:
-				pass
-			print "{0}\t{1}\t{2}".format(sub, pred, obj)
+		# try to utf-8 encode
+		try:
+			encoded_line = each_line.encode('utf-8')
+		except Exception as e:
+			continue
+		else:
+			data = encoded_line.strip().split(' ')
+			if len(data) == 4:
+				try:
+					sub, pred, obj, term = data
+				except Exception as e:
+					pass
+				else:
+					pass
+				finally:
+					pass
+				print "{0}\t{1}\t{2}".format(sub, pred, obj)
 
-		elif len(data) == 3:
-			try:
-				sub, pred, obj = data
-			except Exception as e:
-				pass
-			else:
-				pass
-			finally:
-				pass
-			print "{0}\t{1}\t{2}".format(sub, pred, obj)
+			elif len(data) == 3:
+				try:
+					sub, pred, obj = data
+				except Exception as e:
+					pass
+				else:
+					pass
+				finally:
+					pass
+				print "{0}\t{1}\t{2}".format(sub, pred, obj)
+		
